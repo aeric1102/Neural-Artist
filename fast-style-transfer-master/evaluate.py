7f64f8768a49e488b89828bbcebd062044e4a33e
Your Name <you@example.com>
2019-08-16 20:39:25 +0800

    py modify

--- fast-style-transfer-master/evaluate.py
+++ fast-style-transfer-master/evaluate.py
@@ -11,62 +11,9 @@ import time
 import json
 import subprocess
 import numpy
-from moviepy.video.io.VideoFileClip import VideoFileClip
-import moviepy.video.io.ffmpeg_writer as ffmpeg_writer
 
 BATCH_SIZE = 4
-DEVICE = '/gpu:0'
-
-
-def ffwd_video(path_in, path_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
-    video_clip = VideoFileClip(path_in, audio=False)
-    video_writer = ffmpeg_writer.FFMPEG_VideoWriter(path_out, video_clip.size, video_clip.fps, codec="libx264",
-                                                    preset="medium", bitrate="2000k",
-                                                    audiofile=path_in, threads=None,
-                                                    ffmpeg_params=None)
-
-    g = tf.Graph()
-    soft_config = tf.ConfigProto(allow_soft_placement=True)
-    soft_config.gpu_options.allow_growth = True
-    with g.as_default(), g.device(device_t), \
-            tf.Session(config=soft_config) as sess:
-        batch_shape = (batch_size, video_clip.size[1], video_clip.size[0], 3)
-        img_placeholder = tf.placeholder(tf.float32, shape=batch_shape,
-                                         name='img_placeholder')
-
-        preds = transform.net(img_placeholder)
-        saver = tf.train.Saver()
-        if os.path.isdir(checkpoint_dir):
-            ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
-            else:
-                raise Exception("No checkpoint found...")
-        else:
-            saver.restore(sess, checkpoint_dir)
-
-        X = np.zeros(batch_shape, dtype=np.float32)
-
-        def style_and_write(count):
-            for i in range(count, batch_size):
-                X[i] = X[count - 1]  # Use last frame to fill X
-            _preds = sess.run(preds, feed_dict={img_placeholder: X})
-            for i in range(0, count):
-                video_writer.write_frame(np.clip(_preds[i], 0, 255).astype(np.uint8))
-
-        frame_count = 0  # The frame count that written to X
-        for frame in video_clip.iter_frames():
-            X[frame_count] = frame
-            frame_count += 1
-            if frame_count == batch_size:
-                style_and_write(frame_count)
-                frame_count = 0
-
-        if frame_count != 0:
-            style_and_write(frame_count)
-
-        video_writer.close()
-
+DEVICE = '/cpu:0'
 
 # get img_shape
 def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
